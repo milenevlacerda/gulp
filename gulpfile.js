38bdc58128baceba0e6f8a9a0e658ccccd51990f
milenevlacerda <milenelacerda@icloud.com>
2017-01-09 21:28:58 -0200

    Otimizando imagens

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,31 @@
 var gulp = require( 'gulp' ),
-    imagemin = require( 'gulp-imagemin');
+    imagemin = require( 'gulp-imagemin'),
+    clean = require( 'gulp-clean' );
 
 
+gulp.task( 'copy', [ 'clean' ], function() {
 
-gulp.task( 'build-img', function(){
+    gulp.src( 'src/**/*' )
+        .pipe(
+            gulp.dest( 'dist' )
+        );
+});
 
-    gulp.src( 'src/img/**/*' )
+gulp.task( 'clean', function() {
+
+    gulp.src( 'dist' )
+        .pipe(
+            clean()
+        );
+
+});
+
+gulp.task( 'build-img', function() {
+
+    gulp.src( 'dist/img/**/*' )
         .pipe(
             imagemin()
         )
-        .pipe( gulp.dest( 'src/img' ) );
+        .pipe( gulp.dest( 'dist/img' ) );
 
 });
