ea5cfc831eb0c00b776fe39e0a4a12dbec32218e
milenevlacerda <milenelacerda@icloud.com>
2017-01-09 19:07:09 -0200

    Adicionando .gitignore, tarefa de otimização de imagens, package.json e diretório src

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require( 'gulp' ),
+    imagemin = require( 'gulp-imagemin');
+
+
+
+gulp.task( 'build-img', function(){
+
+    gulp.src( 'src/img/**/*' )
+        .pipe(
+            imagemin()
+        )
+        .pipe( gulp.dest( 'src/img' ) );
+
+});
