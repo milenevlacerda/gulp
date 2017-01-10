c63ccb5fc869f8530ba69be5ad6da2a1a8fae735
milenevlacerda <milenelacerda@icloud.com>
2017-01-10 03:28:01 -0200

    Aplicando jshint, jshintStylish e csslint

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,15 @@
 var gulp = require( 'gulp' ),
-    imagemin    = require( 'gulp-imagemin'),
-    clean       = require( 'gulp-clean' ),
-    concat      = require( 'gulp-concat' ),
-    htmlReplace = require ( 'gulp-html-replace' ),
-    uglify      = require( 'gulp-uglify' ),
-    usemin      = require( 'gulp-usemin' ),
-    cssmin      = require( 'gulp-cssmin' ),
-    browserSync = require( 'browser-sync' );
+    imagemin      = require( 'gulp-imagemin'),
+    clean         = require( 'gulp-clean' ),
+    concat        = require( 'gulp-concat' ),
+    htmlReplace   = require ( 'gulp-html-replace' ),
+    uglify        = require( 'gulp-uglify' ),
+    usemin        = require( 'gulp-usemin' ),
+    cssmin        = require( 'gulp-cssmin' ),
+    browserSync   = require( 'browser-sync' ),
+    jshint        = require( 'gulp-jshint' ),
+    jshintStylish = require( 'jshint-stylish' ),
+    csslint = require( 'gulp-csslint' );
 
 
 // Tarefa default
@@ -67,6 +70,18 @@ gulp.task( 'server', function(){
         server: {
             baseDir: 'src'
         }
+    });
+
+    gulp.watch( 'src/js/*.js' ).on( 'change', function( event ){
+        gulp.src( event.path )
+            .pipe( jshint() )
+            .pipe( jshint.reporter( jshintStylish ) );
+    });
+
+    gulp.watch( 'src/css/*.css' ).on( 'change', function( event ){
+        gulp.src( event.path )
+            .pipe( csslint() )
+            .pipe( csslint.formatter() );
     });
 
     gulp.watch( 'src/**/*' ).on( 'change', browserSync.reload );
