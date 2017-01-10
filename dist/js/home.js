e19fe033b0e1043b4ba35db3c346ebf9ecf2f23a
milenevlacerda <milenelacerda@icloud.com>
2017-01-10 04:45:13 -0200

    Rodando Gulp

--- dist/js/home.js
+++ dist/js/home.js
@@ -2,10 +2,10 @@ document.getElementById('form-busca').onsubmit = function() {
   if (document.getElementById('q').value === '') {
   	  document.getElementById('form-busca').style.background = 'red';
   	  return false;
-  }	
+  }
 };
 
-var banners = ["img/destaque-home-2.png", "img/destaque-home.png"];
+var banners = ["img/destaque-home-2.png"];
 function trocaBanner() {
 	document.querySelector('.destaque img').src = banners[0];
 	banners.reverse();
