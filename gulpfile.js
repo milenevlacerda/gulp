7fce886ceced9c3f38a5314bafd5cdc14900f8e5
milenevlacerda <milenelacerda@icloud.com>
2017-01-10 00:31:51 -0200

    Escrevendo tarefas de minificar imagens, js e css, limpar diretórios e trocar html

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,60 @@
 var gulp = require( 'gulp' ),
-    imagemin = require( 'gulp-imagemin'),
-    clean = require( 'gulp-clean' );
+    imagemin    = require( 'gulp-imagemin'),
+    clean       = require( 'gulp-clean' ),
+    concat      = require( 'gulp-concat' ),
+    htmlReplace = require ( 'gulp-html-replace' ),
+    uglify      = require( 'gulp-uglify' ),
+    usemin      = require( 'gulp-usemin' ),
+    cssmin      = require( 'gulp-cssmin' );
 
+
+// Tarefa default
+
+gulp.task( 'default', [ 'copy' ], function(){
+
+    gulp.start( 'build-img', 'usemin' );
+
+});
+
+// Copiando diretório src
 
 gulp.task( 'copy', [ 'clean' ], function() {
 
-    gulp.src( 'src/**/*' )
-        .pipe(
-            gulp.dest( 'dist' )
-        );
+    return gulp.src( 'src/**/*' )
+    .pipe(
+        gulp.dest( 'dist' )
+    );
 });
+
+// Limpando dist
 
 gulp.task( 'clean', function() {
 
-    gulp.src( 'dist' )
+    return gulp.src( 'dist' )
         .pipe(
             clean()
         );
-
 });
+
+// Minificando imagens
 
 gulp.task( 'build-img', function() {
 
     gulp.src( 'dist/img/**/*' )
-        .pipe(
-            imagemin()
-        )
-        .pipe( gulp.dest( 'dist/img' ) );
+    .pipe(
+        imagemin()
+    )
+    .pipe( gulp.dest( 'dist/img' ) );
 
+});
+
+// Minificando CSS e JS
+
+gulp.task( 'usemin', function(){
+    gulp.src( 'dist/**/*.html' )
+    .pipe( usemin({
+        'js': [ uglify ],
+        'css': [ cssmin ]
+    }))
+    .pipe( gulp.dest( 'dist/' ));
 });
